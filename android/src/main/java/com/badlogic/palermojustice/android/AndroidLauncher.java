860438c3610b27a992063623f7e115fea08037cb
panta <panta-99@live.com>
2025-03-19 16:00:52 +0100

    project skeleton

---
+++ android/src/main/java/com/badlogic/palermojustice/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.badlogic.palermojustice.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.palermojustice.main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new main(), configuration);
+    }
+}
