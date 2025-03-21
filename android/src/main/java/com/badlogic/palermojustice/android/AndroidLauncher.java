a27c661d70f0892338db8c7c2f7829e9637568d7
panta <panta-99@live.com>
2025-03-21 13:59:40 +0100

    readmeFile

--- android/src/main/java/com/badlogic/palermojustice/android/AndroidLauncher.java
+++ android/src/main/java/com/badlogic/palermojustice/android/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.badlogic.palermojustice.main;
+import com.badlogic.palermojustice.Main;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
@@ -13,6 +13,6 @@ public class AndroidLauncher extends AndroidApplication {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
         configuration.useImmersiveMode = true; // Recommended, but not required.
-        initialize(new main(), configuration);
+        initialize(new Main(), configuration);
     }
 }
