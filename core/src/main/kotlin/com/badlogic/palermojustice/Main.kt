5f49b924443546965543778e4eb1a46bfcf13cfc
panta <panta-99@live.com>
2025-03-21 15:08:33 +0100

    orientation: allSensors (free rotation of the screen) GrameScreen: Added a tiny screen string

--- core/src/main/kotlin/com/badlogic/palermojustice/Main.kt
+++ core/src/main/kotlin/com/badlogic/palermojustice/Main.kt
@@ -4,8 +4,21 @@ package com.badlogic.palermojustice
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.GlyphLayout
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class Main : ApplicationAdapter() {
+
+    private lateinit var batch: SpriteBatch
+    private lateinit var font: BitmapFont
+    private lateinit var layout: GlyphLayout
+
+    override fun create() {
+        batch = SpriteBatch()
+        font = BitmapFont() // Usa il font predefinito
+        layout = GlyphLayout()
+    }
     //private lateinit var player: Player
 
     /*override fun create() {
@@ -14,10 +27,27 @@ class Main : ApplicationAdapter() {
     }*/
 
     override fun render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) // Pulisce lo schermo
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) // Clear
+
+        batch.begin()
+
+        val text = "Hello, Mafia Game!"
+        layout.setText(font, text)
+
+        val textWidth = layout.width
+        val textHeight = layout.height
+
+        val x = (Gdx.graphics.width - textWidth) / 2
+        val y = (Gdx.graphics.height + textHeight) / 2
+
+        font.draw(batch, layout, x, y)
+
+        batch.end()
     }
 
     override fun dispose() {
         println("Game Over!")
+        batch.dispose()
+        font.dispose()
     }
 }
