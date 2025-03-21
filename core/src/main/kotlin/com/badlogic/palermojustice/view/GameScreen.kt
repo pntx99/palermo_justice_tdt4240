d0824cf99295342be0560c925d435a2d70af0db1
panta <panta-99@live.com>
2025-03-21 14:52:24 +0100

    orientation: portrait GrameScreen: testing functionalities

--- core/src/main/kotlin/com/badlogic/palermojustice/view/GameScreen.kt
+++ core/src/main/kotlin/com/badlogic/palermojustice/view/GameScreen.kt
@@ -1,4 +1,34 @@
 package com.badlogic.palermojustice.view
 
-class GameScreen {
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class MainScreen : Screen {
+
+    private lateinit var batch: SpriteBatch
+    private lateinit var img: Texture
+
+    override fun show() {
+        batch = SpriteBatch()
+        img = Texture("resources/images/sample.png") // Assicurati che l'immagine esista
+    }
+
+    override fun render(delta: Float) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) // Pulizia dello schermo
+        batch.begin()
+        batch.draw(img, 100f, 100f) // Disegna l'immagine a (100,100)
+        batch.end()
+    }
+
+    override fun resize(width: Int, height: Int) {}
+    override fun pause() {}
+    override fun resume() {}
+    override fun hide() {}
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
 }
