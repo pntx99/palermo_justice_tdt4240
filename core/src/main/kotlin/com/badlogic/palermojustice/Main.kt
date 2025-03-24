cd8606c87450bf9151dbbdc4622e492966c6c6e1
panta <panta-99@live.com>
2025-03-24 13:32:40 +0100

    Main and gameScreen

--- core/src/main/kotlin/com/badlogic/palermojustice/Main.kt
+++ core/src/main/kotlin/com/badlogic/palermojustice/Main.kt
@@ -1,55 +1,10 @@
 package com.badlogic.palermojustice
-//import com.badlogic.palermojustice.Player
 
-import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.g2d.BitmapFont
-import com.badlogic.gdx.graphics.g2d.GlyphLayout
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.Game
+import com.badlogic.palermojustice.view.GameScreen
 
-class Main : ApplicationAdapter() {
-
-    private lateinit var batch: SpriteBatch
-    private lateinit var font: BitmapFont
-    private lateinit var layout: GlyphLayout
-
+class Main : Game() {
     override fun create() {
-        batch = SpriteBatch()
-        font = BitmapFont() // Usa il font predefinito
-        layout = GlyphLayout()
-    }
-    //private lateinit var player: Player
-
-    /*override fun create() {
-        player = Player("ISPETTORE", 100)
-        println("Gioco avviato! Il giocatore è ${player.name} con ${player.health} HP.")
-    }*/
-
-    override fun render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) // Clear
-
-        batch.begin()
-
-        val title = "Palermo Justice"
-        layout.setText(font, title)
-
-        val textWidth = layout.width
-        val textHeight = layout.height
-
-        val x = (Gdx.graphics.width - textWidth) / 2
-        val y = (Gdx.graphics.height + textHeight) / 2
-
-        font.getData().setScale(6F, 6F);
-
-        font.draw(batch, layout, x, y)
-
-        batch.end()
-    }
-
-    override fun dispose() {
-        println("Game Over!")
-        batch.dispose()
-        font.dispose()
+        setScreen(GameScreen()) // Start GameScreen for the main screen
     }
 }
--- core/src/main/kotlin/com/badlogic/palermojustice/view/GameScreen.kt
+++ core/src/main/kotlin/com/badlogic/palermojustice/view/GameScreen.kt
@@ -3,32 +3,92 @@ package com.badlogic.palermojustice.view
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
+//import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.*
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.viewport.ScreenViewport
 
-class MainScreen : Screen {
+class GameScreen : Screen {
+    private val stage = Stage(ScreenViewport())
+    private val batch = SpriteBatch()
+    private val font = BitmapFont()
+    //private val background = Texture("background.png")
+    private lateinit var titleLabel: Label
+    private lateinit var createButton: TextButton
+    private lateinit var joinButton: TextButton
+    private lateinit var inputField: TextField
 
-    private lateinit var batch: SpriteBatch
-    private lateinit var img: Texture
+    override fun show() { //
+        Gdx.input.inputProcessor = stage
 
-    override fun show() {
-        batch = SpriteBatch()
-        img = Texture("resources/images/sample.png")
+        // Font style
+        val labelStyle = Label.LabelStyle()
+        labelStyle.font = font
+
+        // Title
+        titleLabel = Label("Palermo Justice", labelStyle)
+        titleLabel.setFontScale(6f)
+        titleLabel.setAlignment(Align.center)
+
+        // Input fields
+        val textFieldStyle = TextField.TextFieldStyle()
+        textFieldStyle.font = font
+        textFieldStyle.fontColor = com.badlogic.gdx.graphics.Color.WHITE
+        inputField = TextField("", textFieldStyle)
+
+        // Buttons
+        val buttonStyle = TextButton.TextButtonStyle()
+        buttonStyle.font = font
+
+        createButton = TextButton("Create Game", buttonStyle)
+        joinButton = TextButton("Join", buttonStyle)
+
+        // Listener for buttons
+        createButton.addListener(object : ClickListener() {
+            override fun clicked(event: com.badlogic.gdx.scenes.scene2d.InputEvent?, x: Float, y: Float) {
+                println("Create Game clicked")
+            }
+        })
+
+        joinButton.addListener(object : ClickListener() {
+            override fun clicked(event: com.badlogic.gdx.scenes.scene2d.InputEvent?, x: Float, y: Float) {
+                println("Join Game with code: ${inputField.text}")
+            }
+        })
+
+        // Layout with Table
+        val table = Table()
+        table.setFillParent(true)
+        table.add(titleLabel).padBottom(20f).row()
+        //table.add(Image(background)).size(200f, 120f).padBottom(20f).row()
+        table.add(createButton).padBottom(10f).row()
+        table.add(inputField).size(150f, 40f).padBottom(10f).row()
+        table.add(joinButton)
+
+        stage.addActor(table)
     }
 
     override fun render(delta: Float) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 100f, 100f)
-        batch.end()
+        stage.act(delta)
+        stage.draw()
     }
 
-    override fun resize(width: Int, height: Int) {}
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
+    }
+
+    override fun hide() {}
     override fun pause() {}
     override fun resume() {}
-    override fun hide() {}
     override fun dispose() {
+        stage.dispose()
         batch.dispose()
-        img.dispose()
+        font.dispose()
+        //background.dispose()
     }
 }
